5cb7e0971c45527bc8045b14837331654fd5d40f
MasterHongYu <mhongyu951023@gmail.com>
2023-11-04 23:13:46 +0800

    ㄅ

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,6 +1,17 @@
-var a = 0
-var n = prompt("你要哪一位數？")
-for (var i=0;i <= n;i++){
-    a += i
+var quation = [
+    {Q:"sin θ + cos θ = ? (A) 0.5 (B) 1 (C) 1.5",A:"B"},
+    {Q:"sin θ + cos θ = ? (A) 0.5 (B) 1 (C) 1.5",A:"B"},
+    {Q:"sin θ + cos θ = ? (A) 0.5 (B) 1 (C) 1.5",A:"B"}
+]
+var score = 0
+for (var i=0;i<3;i++){
+    inputAnswer = prompt(quation[i].Q)
+    if (inputAnswer == quation[i].A){
+        score++
+        alert("you right")
+    }
+    else {
+        alert("you wrong")
+    }
 }
-document.write(a)
+alert("your finally score is"+ score)
